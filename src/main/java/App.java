97dcbee863d88cc0696e6f9c6b0dd5009d2abdcd
rvilyin <floyslife@yandex.ru>
2023-12-20 08:32:24 +0300

    made task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,10 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = (Cat) applicationContext.getBean("catBean");
+        Cat cat2 = (Cat) applicationContext.getBean("catBean");
+        System.out.println("HelloWorld bean и bean2 ссылаются на один и тот же объект? " + (bean == bean2));
+        System.out.println("cat1 и cat2 ссылаются на один и тот же объект? " + (cat1 == cat2));
     }
 }
